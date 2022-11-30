4074a6d9c77aa8d159c0c4c18133bf5ce53f2a62
nilufarxon <orifjonova07@inbox.ru>
2022-11-30 16:31:50 +0500

    login

---
+++ js/app.js
@@ -0,0 +1,12 @@
+function show() {
+  let pswrd = document.getElementById("pswrd");
+  let icon = document.querySelector(".fas");
+  if (pswrd.type === "password") {
+    pswrd.type = "text";
+    pswrd.style.marginTop = "20px";
+    icon.style.color = "#7f2092";
+  } else {
+    pswrd.type = "password";
+    icon.style.color = "grey";
+  }
+}
